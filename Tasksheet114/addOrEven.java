5df5ea9f5bc5e247520a5e753b1fd56314df6bd1
alexlangtabi <68006914+alexlangtabi@users.noreply.github.com>
2024-10-08 11:28:58 +0000

    Add or Even

---
+++ Tasksheet114/addOrEven.java
@@ -0,0 +1,19 @@
+package Tasksheet114;
+
+public class addOrEven {
+    public static void main(String[] args) {
+    int check_number = 10;
+           
+    //String message;?????
+       
+        for (int i = 1; i <=check_number; i++) {
+           
+        if (i% 2 == 0) {
+                System.out.println(i + " is even number" );
+            } else {
+                System.out.println(i + " is odd number" );
+            }                         
+        }
+   
+    }
+}
